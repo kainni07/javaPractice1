e550984ecea2fecbc78de7a4c83323ded93e5cf6
kainni07 <49780516+kainni07@users.noreply.github.com>
2019-09-10 07:11:07 +0900

    Create JavaPractice1.java

---
+++ JavaPractice1.java
@@ -0,0 +1,13 @@
+ class JavaPractice1 {
+     
+     public static void main(String args[]) {
+        int n = 10;
+        if (n == 0) {
+            System.out.println("0です");
+        } else if (n > 0) {
+            System.ou.println("正です");
+        } else {
+            System.out.println("負です");
+        }
+     }
+ }
